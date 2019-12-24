bd2e3b9e5b0708896fa011db7f3e7ebfe88d300d
Hugo Jeller <hugo_jeller@hotmail.com>
2019-12-24 12:31:16 +0000

    added bzip

--- app/Console/Commands/CalculateCompression.php
+++ app/Console/Commands/CalculateCompression.php
@@ -41,6 +41,8 @@ class CalculateCompression extends Command
 		$current = 0;
 		$gzip = 0;
 		$gzip2 = 0;
+		$bzip = 0;
+		$bzip2 = 0;
 
 		$cursor = Session::query()->cursor();
 
@@ -49,13 +51,19 @@ class CalculateCompression extends Command
 			$data = $session->metrics;
 			$gzipped = gzdeflate($data, 9);
 			$gzipped2 = gzdeflate($gzipped, 9);
+			$bzipped = bzcompress($data, 9);
+			$bzipped2 = bzcompress($bzipped, 9);
 
 			$current += strlen($data);
 			$gzip += strlen($gzipped);
 			$gzip2 += strlen($gzipped2);
+			$bzip += strlen($bzipped);
+			$bzip2 += strlen($bzipped2);
 		}
 
 		$this->info("Total raw data: $current bytes");
+		$this->info("Total bzip (1x): $bzip bytes");
+		$this->info("Total bzip (2x): $bzip2 bytes");
 		$this->info("Total gzip (1x): $gzip bytes");
 		$this->info("Total gzip (2x): $gzip2 bytes");
 	}
