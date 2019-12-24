7efe2396579900a343aed91323987f47ddbedfd7
Hugo Jeller <hugo_jeller@hotmail.com>
2019-12-24 18:34:33 +0000

    cleaned session view

--- resources/views/session.blade.php
+++ resources/views/session.blade.php
@@ -10,6 +10,9 @@
     hs% -> hs count
     kdr -> kills
     
+    top 5 guns deaths
+    damage taken
+    
 -->
 
 @php
@@ -18,26 +21,25 @@
 @endphp
 
 @section('content')
-    <div class="bg-grey-800 mx-32 py-6 px-16 rounded-lg shadow-lg">
+    <div class="bg-grey-800 mx-32 py-6 px-16 shadow-lg">
+        <h1 class="mb-8 font-hairline text-grey-500 text-center text-3xl tracking-wider">Session #{{ $session->id }} statistics</h1>
         <div class="flex flex-row">
             <!-- Top guns -->
             <div class="w-1/3">
                 <h2 class="p-2 mb-4 font-light text-center text-2xl tracking-wider uppercase">Top weapons</h2>
                 <table>
-                    @foreach ($data['kills-by-weapon'] as $weapon => $hits)
+                    @foreach ($data['damage-by-weapon'] as $weapon => $damage)
                         <tr>
                             <td class="font-medium text-3xl text-grey-500">{{ $loop->index + 1}}</td>
-                            <td class="px-3 font-csgo text-center text-2xl text-grey-700">
+                            <td class="px-3 font-csgo text-center text-2xl text-grey-400">
                                 {{ csgo_getchar_by_name($weapon) }}
                             </td>
                             <td class="">
                                 <div class="">
                                     <p class="font-medium uppercase">{{ $weapon }}</p>
                                     <p class="text-grey-600 font-normal tracking-tight">
-                                        {{ $hits }} kills
-                                        @if($data['damage-by-weapon'][$weapon] ?? false)
-                                            <small>({{ $data['damage-by-weapon'][$weapon] }} damage)</small>
-                                        @endif
+                                        {{ $data['kills-by-weapon'][$weapon] ?? 0 }} kills
+                                        <small>({{ $damage }} damage)</small>
                                     </p>
                                 </div>
                             </td>
@@ -77,41 +79,49 @@
                 <div class="flex flex-wrap">
                     <!-- Kills/Min -->
                     <div class="flex flex-col w-1/2 justify-center p-3 text-center">
-                        <h3 class="text-grey-500 text-xl font-light uppercase">KILLS / MIN</h3>
+                        <h3 class="text-grey-400 text-xl font-light tracking-wide uppercase">KILLS / MIN</h3>
                         <h2 class="text-grey-200 font-normal text-3xl tracking-tight">
                             {{ number_format($data['kills-per-min'], 1) ?? 'N/A' }}
                         </h2>
-                        <small class="text-grey-400 text-base font-light tracking-tight">{{ $data['duration'] ?? 'N/A' }} minutes</small>
+                        <small class="text-grey-500 text-sm font-light tracking-tight">{{ $data['duration'] ?? 'N/A' }} minutes</small>
                     </div>
                     
                     <!-- Damage/Min -->
                     <div class="flex flex-col w-1/2 justify-center p-3 text-center">
-                        <h3 class="text-grey-500 text-xl font-light uppercase">DAMAGE / MIN</h3>
+                        <h3 class="text-grey-400 text-xl font-light tracking-wide uppercase">DAMAGE / MIN</h3>
                         <h2 class="text-grey-200 font-normal text-3xl tracking-tight">
                             {{ number_format($data['damage-per-min'], 1) ?? 'N/A' }}
                         </h2>
-                        <small class="text-grey-400 text-base font-light tracking-tight">{{ $data['damage-total'] ?? 'N/A' }} damage</small>
+                        <small class="text-grey-500 text-sm font-light tracking-tight">{{ $data['damage-total'] ?? 'N/A' }} damage</small>
                     </div>
                     
                     <!-- KDR -->
                     <div class="flex flex-col w-1/2 justify-center p-3 text-center">
-                        <h3 class="text-grey-500 text-xl font-light uppercase">KDR</h3>
+                        <h3 class="text-grey-400 text-xl font-light tracking-wide uppercase">KDR</h3>
                         <h2 class="text-grey-200 font-normal text-3xl tracking-tight">
                             {{ number_format($data['kdr'], 1) }}
                         </h2>
-                        <small class="text-grey-400 text-base font-light tracking-tight">{{ $data['kills-total'] ?? 'N/A' }} kills</small>
+                        <small class="text-grey-500 text-sm font-light tracking-tight">{{ $data['kills-total'] ?? 'N/A' }} kills</small>
                     </div>
                     
                     <!-- HS% -->
                     <div class="flex flex-col w-1/2 justify-center p-3 text-center">
-                        <h3 class="text-grey-500 text-xl font-light uppercase">HS %</h3>
+                        <h3 class="text-grey-400 text-xl font-light tracking-wide uppercase">HS %</h3>
                         <h2 class="text-grey-200 font-normal text-3xl tracking-tight">
                             {{ number_format($data['hs-percentage'], 1) }}%
                         </h2>
-                        <small class="text-grey-400 text-base font-light tracking-tight">{{ $data['kills-by-hitgroup']['head'] ?? 0 }} headshots</small>
+                        <small class="text-grey-500 text-sm font-light tracking-tight">{{ $data['kills-by-hitgroup']['head'] ?? 0 }} headshots</small>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="flex mt-8 justify-center">
+            <a href="{{ route('search') }}" class="trans-fast flex items-center justify-center
+                    h-16 w-32
+                    font-mono text-lg
+                    text-blue-100 hover:text-white
+                    no-underline
+                    bg-blue-800 hover:bg-blue-700 hover:shadow">< Back</a>
         </div>
     </div>
 @endsection
